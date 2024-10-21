2cf3008de3fc99235fdde447dd670979b274c10e
missynancy <missynancy722@outlook.com>
2024-10-21 21:08:46 +0300

    home-page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,16 @@ import { Background } from '../components/hero/Background';
 import { Hero } from '../components/hero/Hero';
 import { Latest } from './Latest/Latest';
 import { New } from './New';
-import image1 from "/homeimages/Double_Monk_Strap1-removebg-preview.png"
-import image2 from '/homeimages/Channel_two-tone_slingbacks_4-removebg-preview.png'
-import image3 from "/homeimages/Unisex_LED_Luminous_Toller_Shoe_1-removebg-preview.png";
-import image4 from '/homeimages/sandals-removebg-preview.png'
-import image5 from '/homeimages/Converse_Run_Star_Hike_Hi_166799c_Unisex_White_Black_Canvas_Sneaker_Shoes_Nr6211__8___Adult_Unisex-removebg-preview.png'
-import image6 from '/homeimages/sneaker-removebg-preview.png'
+import img1 from '/images/phones-removebg-preview.png';
+import img2 from '/images/laptops-removebg-preview.png';
+import img3 from '/images/desktops-removebg-preview.png';
+import img4 from '/images/monitor-removebg-preview.png';
+import img5 from '/images/ups-removebg-preview.png';
+import img6 from '/images/CA-removebg-preview.png';
+import img7 from '/images/PA-removebg-preview.png';
+import img8 from '/images/tv-removebg-preview.png';
+import img9 from '/images/printers-removebg-preview.png';
+import { Shop } from './shop/Shop';
 
 
 export const Home = () => {
@@ -49,120 +53,127 @@ export const Home = () => {
   return (
     <>
       <div className="landing-page">
-      <div className="home">
-        <>
-        <div className="backcircle"></div>
-        <Background heroCount={heroCount}/> 
-        <Hero 
-         heroData = {heroData[heroCount]}
-         heroCount = {heroCount} 
-         setHeroCount = {setHeroCount}
-         
-          />
-        </>
-        
-        
-      </div>
-      <div className="home-links">
-        <div className="links-1">
-          <div className="links-1-2">
-            <div className="links-1-2-1">
-              <img src={image4} />
-              <div className="link-1-content">
-                  <h3>Sandals</h3>
-                  <Link to='/sandals'>Shop more</Link>
-              </div>
-            </div>
-            <div className="links-1-2-1">
-              <img src={image5} />
-              <div className="link-1-content">
-                  <h3>Converse</h3>
-                  <Link to='/converse'>Shop more</Link>
-              </div>
-            </div>
-          </div>
-          <div className="links-1-1">
-            <img src={image6} />
-            <div className="link-1-content">
-                <h3>Sneakers</h3>
-                <Link to='/sneakers'>Shop more</Link>
-            </div>
-          </div>
+        <div className="home">
+          <>
+          <div className="backcircle"></div>
+          <Background heroCount={heroCount}/> 
+          <Hero 
+          heroData = {heroData[heroCount]}
+          heroCount = {heroCount} 
+          setHeroCount = {setHeroCount}
+          
+            />
+          </>
+          
+          
         </div>
-        <div className="links-1">
-          <div className="links-1-1">
-            <img src={image1} />
-            <div className="link-1-content">
-                <h3 to='/officials'>Officials</h3>
-                <Link>Shop more</Link>
+        <div className="home-links">
+          <div className="links-1">
+            <div className="links-1-2-1">
+              <img src={img1} />
+              <div className="link-1-content">
+                <h3>Phones</h3>
+                <Link to='/converse'>Shop more</Link>
+              </div>
+            </div>
+            <div className="links-1-2-1">
+              <img src={img2} />
+              <div className="link-1-content">
+                <h3>Laptops</h3>
+                <Link to='/converse'>Shop more</Link>
+              </div>
+            </div>
+            <div className="links-1-2-1">
+              <img src={img3} />
+              <div className="link-1-content">
+                <h3>Desktops</h3>
+                <Link to='/converse'>Shop more</Link>
+              </div>
+            </div>
+            <div className="links-1-2-1">
+              <img src={img4} />
+              <div className="link-1-content">
+                <h3>Monitors</h3>
+                <Link to='/converse'>Shop more</Link>
+              </div>
             </div>
           </div>
           <div className="links-1-2">
-            <div className="links-1-2-1">
-              <img src={image2} />
+            <div className="links-1-2-2">
+              <img src={img5} />
               <div className="link-1-content">
-                  <h3>Heels</h3>
-                  <Link to='/heels'>Shop more</Link>
+                <h3>UPS/Power backup</h3>
+                <Link to='/converse'>Shop more</Link>
               </div>
             </div>
-            <div className="links-1-2-1">
-              <img src={image3} />
+            <div className="links-1-2-2">
+              <img src={img6} />
               <div className="link-1-content">
-                  <h3>kids</h3>
-                  <Link to='/kids'>Shop more</Link>
+                <h3>Computer Accessories</h3>
+                <Link to='/converse'>Shop more</Link>
+              </div>
+            </div>
+            <div className="links-1-2-2">
+              <img src={img7} />
+              <div className="link-1-content">
+                <h3>Phone Accessories</h3>
+                <Link to='/converse'>Shop more</Link>
+              </div>
+            </div>
+            <div className="links-1-2-2">
+              <img src={img8} />
+              <div className="link-1-content">
+                <h3>TV $ Entertainment</h3>
+                <Link to='/converse'>Shop more</Link>
+              </div>
+            </div>
+            <div className="links-1-2-2">
+              <img src={img9} />
+              <div className="link-1-content">
+                <h3>Printer, Scanners $ Copiers</h3>
+                <Link to='/converse'>Shop more</Link>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className='features'>
-        <h1>Features</h1>
-        <div className='features-container'>
-          {
-            data && data.length > 0 ? 
-            data.map(dataItem => (
-              <div key={dataItem.id} className='items'>
-                <div id='deliveries'
-                  style={{
-                    fontSize: '20px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    padding: '10px 20px',
-                  }}
-                  onClick={() => handleSelection(dataItem.id)}
-                >
-                  <div style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center'
-                  }}>
-                    <h3>{dataItem.icon}</h3>
-                    <i className='bx bx-chevron-right'></i>
+        <div className='features'>
+          <h1>Features</h1>
+          <div className='features-container'>
+            {
+              data && data.length > 0 ? 
+              data.map(dataItem => (
+                <div key={dataItem.id} className='items'>
+                  <div id='deliveries'
+                    style={{
+                      fontSize: '20px',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      padding: '10px 20px',
+                    }}
+                    onClick={() => handleSelection(dataItem.id)}
+                  >
+                    <div style={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center'
+                    }}>
+                      <h3>{dataItem.icon}</h3>
+                      <i className='bx bx-chevron-right'></i>
+                    </div>
                   </div>
+                  {
+                    selectedFeature === dataItem.id ? 
+                    <div>{dataItem.content}</div>
+                    : null
+                  }
                 </div>
-                {
-                  selectedFeature === dataItem.id ? 
-                  <div>{dataItem.content}</div>
-                  : null
-                }
-              </div>
-            ))
-            : (<div>No data present</div>)
-          }
+              ))
+              : (<div>No data present</div>)
+            }
+          </div>
         </div>
-      </div>
+        < Shop  className='h-shop'/>
       
-      <div className="new-latest">
-      <h1>New Arrivals</h1>
-      <div className="latest">
-        <div className="latest-1">
-          < New />
-        </div>
-        <div className="latest-2">
-         <Latest />
-        </div>
-      </div>
-      </div>
       </div>
       
     </>
